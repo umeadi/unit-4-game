09821659c1ab71eaceb299a6714ecf286a89c155
Umeadi, Marlon <umeadi@gmail.com>
2019-05-16 03:03:09 -0500

    added on click jQuery functionality to crystals

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+$(".crystal1").on("click", function () {
+
+    // Clicking the crystal triggers an alert message.
+    alert("You clicked crystal number1!");
+    console.log("You clicked crystal number 1");
+
+});
+
+$(".crystal2").on("click", function () {
+
+    // Clicking the crystal triggers an alert message.
+    alert("You clicked crystal number 2!");
+    console.log("You clikced crystal number 2");
+
+});
+
+$(".crystal3").on("click", function () {
+
+    // Clicking the crystal triggers an alert message.
+    alert("You clicked crystal number 3!");
+    console.log("You clikced crystal number 3");
+
+});
+
+$(".crystal4").on("click", function () {
+
+    // Clicking the crystal triggers an alert message.
+    alert("You clicked crystal number 4!");
+    console.log("You clikced crystal number 4");
+
+});
