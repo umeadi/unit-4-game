64883f75c7432459d2eca62fa4e439281af80fb6
Umeadi, Marlon <umeadi@gmail.com>
2019-05-18 22:50:29 -0500

    added comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,19 @@
+var randomNumber;
+var wins;
+var losses;
+
+console.log(randomNumber);
+
+
+// player will be shown a random number at the start of the game
+// a new random number should be generated upon each win/loss to each crystal
+// every crystal needs to have a random value between one and 12
+// when clicking each crystal, the value should be getting added to the curren total
+// if the total from the crystals equals the random numbner a win will be generated
+// if the totla is larger than the random numnber, a loss will be generated
+//
+
+
 $(".crystal1").on("click", function () {
 
     // Clicking the crystal triggers an alert message.
